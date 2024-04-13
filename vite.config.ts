9d0a6a7669cf5f57a6dcb5a2564c68da72f782e4
Kane <150378@qq.com>
2024-04-13 11:57:08 +0800

    chore: 增加auto-import 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineApplicationConfig } from '@vben/vite-config';
 import Inspector from 'vite-plugin-vue-inspector';
+import AutoImport from 'unplugin-auto-import/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 
 export default defineApplicationConfig({
   overrides: {
@@ -40,6 +43,18 @@ export default defineApplicationConfig({
     plugins: [
       Inspector({
         openInEditorHost: 'http://localhost:5173',
+      }),
+      AutoImport({
+        // dirs: ['src/hooks/**'],
+        imports: ['vue'],
+        // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
+        dts: 'types/auto-import.d.ts',
+      }),
+      Components({
+        // dirs: ["src/components/**"],
+        // ui库解析器，也可以自定义
+        resolvers: [AntDesignVueResolver({ importStyle: false })],
+        dts: 'types/components.d.ts',
       }),
     ],
   },
